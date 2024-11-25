099493efe128eeb9607263f4f926dd208e1408a6
Sushant98K <sushant98k@gmail.com>
2024-11-25 20:02:51 +0530

    add word library

---
+++ word game/word_library.py
@@ -0,0 +1,17 @@
+# word_library.py
+
+letters = [
+    ['h', 'o', 'l', 'i', 'd', 'a', 'y'],  # Related to holiday
+    ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g'],  # Related to programming
+    ['b', 'o', 'o', 't', 'c', 'a', 'm', 'p'],  # Related to camping
+    ['f', 'l', 'o', 'w', 'c', 'h', 'a', 'r', 't'],  # Related to flowchart
+    ['w', 'o', 'r', 'd', 's', 'c', 'a', 'p', 'e', 's']   # Related to wordscapes
+]
+
+words = [
+    ["hi", "hay", "day", "lay", "dal", "lad", "lid", "hold", "lady", "hall"],  # Holiday
+    ["program", "programming", "grim", "ring", "ram", "main", "rain", "pain", "go", "am", "an", "in", "it", "is", "or", "pro", "ram"],  # Programming
+    ["camp", "boat", "coat", "bloom", "mop", "tab", "bat"],  # Camping
+    ["flow", "chart", "loft", "claw", "walt", "halt"],  # Flowchart
+    ["word", "scapes", "sword", "case", "capes", "scape", "scar", "soar"]  # Wordscapes
+]
